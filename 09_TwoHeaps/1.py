79681bb05aa41218e461487067235091a57cd524
lywangj <og21893@bristol.ac.uk>
2022-07-05 22:45:05 +0100

    create 09

---
+++ 09_TwoHeaps/1.py
@@ -0,0 +1,38 @@
+from heapq import *
+
+
+class MedianOfAStream:
+
+  maxHeap = []  # containing first half of numbers
+  minHeap = []  # containing second half of numbers
+
+  def insert_num(self, num):
+    if not self.maxHeap or -self.maxHeap[0] >= num:
+        heappush(self.maxHeap, -num)
+    else:
+        heappush(self.minHeap, num)
+
+    if len(self.maxHeap) > len(self.minHeap) + 1:
+        heappush(self.minHeap, -heappop(self.maxHeap))
+    elif len(self.maxHeap) < len(self.minHeap):
+        heappush(self.maxHeap, -heappop(self.minHeap))
+
+  def find_median(self):
+    if len(self.maxHeap) == len(self.minHeap):
+        return -self.maxHeap[0] / 2.0 + self.minHeap[0] / 2.0
+    else:
+        return -self.maxHeap[0] / 1.0
+
+
+def main():
+  medianOfAStream = MedianOfAStream()
+  medianOfAStream.insert_num(3)
+  medianOfAStream.insert_num(1)
+  print("The median is: " + str(medianOfAStream.find_median()))
+  medianOfAStream.insert_num(5)
+  print("The median is: " + str(medianOfAStream.find_median()))
+  medianOfAStream.insert_num(4)
+  print("The median is: " + str(medianOfAStream.find_median()))
+
+
+main()
---
+++ 09_TwoHeaps/README.md
@@ -0,0 +1,31 @@
+### Find the Median of a Number Stream
+
+Given a series of **num** is handled by two actions step-by-step.
+
+- insertNum(int **num**): stores the numbers
+- findMedian(): returns the median of all numbers inserted in the class, or the average of the middle two numbers, if the count of numbers is even.
+
+- 1. insertNum(3)
+- 2. insertNum(1)
+- 3. findMedian() -> output: 3
+- 4. insertNum(2)
+- 5. findMedian() -> output: 2
+- 6. insertNum(4)
+- 7. findMedian() -> output: 2.5
+
+Task:
+- Design a class which contains one maxheap and minheap to store every inserted number, and methods to calculate the median of a inserted numbers.
+
+Time complexity: O(nlogn)
+- For inserting an item into a heap, the TC will be O(logn)
+- For calculate the median, the TC will be only O(1)
+
+Space complexity: O(n)
+- n as the total number of num
+
+Constraints:
+- 1 <= total number of num <= 100
+- 1 <= num <= 100
+
+<img src="../images/2022-07-05_002241.png" height="280">
+<a class="return" href="../README.md" style="text-align:right;"> 《BACK》 </a>
